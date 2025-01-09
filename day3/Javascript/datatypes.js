9c568c5fa63193a2fa1f79ef74654c89c1fe2c8d
gowtham <gowtham.r2022it@sece.ac.in>
2025-01-09 22:41:34 +0530

    added

---
+++ day3/Javascript/datatypes.js
@@ -0,0 +1,42 @@
+
+ a="sece"
+ console.log(a)
+ b=true
+console.log(b)
+
+ console.log(typeof(a))
+
+ console.log(typeof(b))
+
+ array=['a',10,true,[20,20],"srieshwar"]
+ console.log(array)
+ console.log(typeof(array))
+
+
+ obj={
+    firstname:"Sri",
+    lastname:"eshwar"
+}
+ console.log(obj)
+ console.log(typeof(obj))
+ console.log(obj.firstname)
+ console.log(obj["lastname"])
+
+
+ obj1={}
+console.log(obj1)
+obj1["dept"]="cse"
+ obj1["sec"]="a"
+ console.log(obj1)
+
+ 
+ obj2=new Object()
+ console.log(obj2)
+ obj2.count=70
+ obj2.abs=1    
+ totalPresentCount=obj2.count - obj2.abs
+ console.log( "present",totalPresentCount)
+console.log(obj2)
+set =new Set(["hello",1,2,'sece'])
+console.log(typeof(set))
+console.log(set)
---
+++ day3/operator.js
@@ -0,0 +1,4 @@
+//logical operator
+var a=5
+var b="5"
+console.log(a===b)
